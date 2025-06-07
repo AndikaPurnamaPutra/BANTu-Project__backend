c1f15ee5cbcbf1d6ccff41a9675460451ca5bb70
Ucok <ucok@Ucoks-MacBook-Pro.local>
2025-06-07 20:32:53 +0700

    update admin backend

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['designer', 'artisan', 'admin'], required: true },
+  role: { type: String, enum: ['designer', 'artisan'], required: true },
   profilePic: { type: String },
   bio: { type: String },
   contactInfo: {
